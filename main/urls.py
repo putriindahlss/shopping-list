7d3dc5a5df78a56957758211fda16e0afd547ff0
putriindahlss <putriindah2004@gmail.com>
2023-09-20 15:34:20 +0700

    tutur 3

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from main.views import show_main
-from main.views import show_main, create_product
-from main.views import show_main, create_product, show_xml 
-from main.views import show_main, create_product, show_xml, show_json
 from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import register #sesuaikan dengan nama fungsi yang dibuat
+from main.views import login_user #sesuaikan dengan nama fungsi yang dibuat
+from main.views import logout_user
 
 app_name = 'main'
 
@@ -13,5 +12,8 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'), 
     path('json/', show_json, name='show_json'), 
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
